f4bd96f64c7b344d64b6175cf6232324ac881821
nateGonpass <nateVer2015@csu.fullerton.edu>
2026-02-19 11:29:38 -0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,6 +1,28 @@
+//Nathan Verduzco
+//876414053
+// : == after this initizaliation 
+// ; == 
+#include <iostream>
+#include <string>
 
+template <typename T> class box{
+ private:
+  T item_;
+ 
+public:
+  box(T item):item_(item) {}
+
+  void SetValue(T item) { item_ = item; }
+ T GetValue()
+{ return item_;};
 
 int main() {
-return 0;
+    std::cout << "test here" << "\n";
+  box b_int(5);
+  box b_double(5.0);
+  std::cout << "the int box is " << b_int.GetValue()
+            << " the double box is "
+            << b_double.GetValue();
+  return 0;
 }
 //all code goes in here
